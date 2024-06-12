2280f7da1bacb55c65d4ae5653f0e60741cc9eb8
Magomedmirze Agakhanov <agahanovmaga17@gmail.com>
2024-06-12 21:07:59 +0300

    add dev branch

--- src/homework8/GitTest.java
+++ src/homework8/GitTest.java
@@ -3,6 +3,6 @@ package homework8;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("some code...");
+        System.out.println("Some code...");
     }
 }
